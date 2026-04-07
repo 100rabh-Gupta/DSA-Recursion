974d9ba6998447842421711c2a3bd29231cebbb8
Saurabh Gupta <100rabhgupta8400@gmail.com>
2026-04-07 19:28:46 +0530

    Time: 6 ms (12.70%) | Memory: 10.4 MB (48.59%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int up=0;
+           int l=0;
+             
+    for (char c:moves){
+        if(c=='U'){
+            up++;
+        }
+        if(c=='D'){
+            up--;
+        }
+        if(c=='L'){
+            l++;
+        }
+        if(c=='R'){
+            l--;
+        }
+
+    }
+    return up==0&&l==0;
+        
+    }
+};
